779a85b5320fc8d2efef22f7b6f60d2f9e5d746e
skycity233 <544907049@qq.com>
2019-03-25 12:42:40 +0800

    添加读取数据

---
+++ JudgeSystem.py
@@ -0,0 +1,29 @@
+# encoding = utf-8
+"""
+@version: 0.1
+@author: JackWang
+@file: JudgeSystem
+@time: 2019/03/25 上午10:41
+"""
+
+from ReadInput import *
+
+
+def read_all(path):
+    data_car = read(path + 'car' + '.txt')
+    data_cross = read(path + 'cross' + '.txt')
+    data_road = read(path + 'road' + '.txt')
+    return data_car, data_cross, data_road
+
+
+def judge():
+    map = '1'
+    path = 'data/' + map + '/'
+    data_car, data_cross, data_road = read_all(path)
+
+
+# todo(JackWang): compete this code
+
+
+if __name__ == '__main__':
+    judge()
---
+++ ReadInput.py
@@ -0,0 +1,30 @@
+# encoding = utf-8
+"""
+@version: 0.1
+@author: JackWang
+@file: ReadInput
+@time: 2019/03/25 上午10:48
+"""
+
+
+def read(path):
+
+    with open(path) as input_file:
+        input_data = input_file.readlines()
+        global data
+        if input_data[0][5] == 'f':
+            data = [_id, _from, to, speed, planTime] = [[], [], [], [], []]
+        elif input_data[0][5] == 'r':
+            data = [_id, roadId, roadId, roadId, roadId] = [[], [], [], [], []]
+        elif input_data[0][5] == "l":
+            data = [_id, length, speed, channel, _from, to, isDuplex] = [[], [], [], [], [], [], []]
+
+    for eachline in input_data:
+        if eachline.startswith("#"):
+            continue
+        eachline = eachline.strip('\n').strip('()')
+        data_t = eachline.split(",")
+        for i in range(len(data_t)):
+            data[i].append(int(data_t[i]))
+
+    return data
